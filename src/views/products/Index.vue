b98b7be494285d0da02762f0390061b0354cc944
chetan <chetankharel7@gmail.com>
2021-03-23 21:53:52 +0545

    products from API

--- src/views/products/Index.vue
+++ src/views/products/Index.vue
@@ -18,13 +18,13 @@
         <v-col cols="12" md="8">
           <v-row>
             <v-col :key="product.id" v-for="product in products" cols="12" md="4">
-              <v-card class="mx-auto" max-width="400">
+              <v-card :to="product.productLink" exact class="mx-auto" max-width="400">
                 <v-img
                   src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
                   height="200px"
                 ></v-img>
 
-                <v-card-title> {{ product.productName }} </v-card-title>
+                <v-card-title>{{ product.productName }}</v-card-title>
 
                 <v-card-subtitle> {{ product.price }} </v-card-subtitle>
 
@@ -41,45 +41,21 @@
 </template>
 
 <script>
-import Api from '@/services/Api'
+import ProductService from '@/services/ProductService'
 
 export default {
-  mounted() {
+  async mounted() {
+     try {
+       const response = await ProductService.all()
+       this.products = response.data.data
+     }
+     catch (e) {
+
+     }
   },
   data() {
     return {
-      products: [
-        {
-           id: 1,
-           productName: 'Earphone',
-           price: 100,
-           source: 'Daraz'
-        },
-         {
-           id: 2,
-           productName: 'Water Bottle',
-           price: 400,
-           source: 'Sastodeal'
-        },
-         {
-           id: 3,
-           productName: 'T-shirt',
-           price: 500,
-           source: 'Daraz'
-        },
-         {
-           id: 4,
-           productName: 'Lenovo Laptop',
-           price: 50000,
-           source: 'Sastodeal'
-        },
-         {
-           id: 5,
-           productName: 'Mobile',
-           price: 10000,
-           source: 'Daraz'
-        }
-      ]
+      products: []
     }
   }
 };
